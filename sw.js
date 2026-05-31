c8c3366db9190e60a1c129111f8c31ad6728e1ad
kurororo25-hash <kurororo25@gmail.com>
2026-05-31 14:31:19 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,8 +1,16 @@
-const CACHE = "taikin-webapp-v1";
+const CACHE = "taikin-webapp-v2";
 const FILES = ["./", "./index.html", "./manifest.webmanifest"];
 self.addEventListener("install", e => {
+  self.skipWaiting();
   e.waitUntil(caches.open(CACHE).then(c => c.addAll(FILES)));
 });
+self.addEventListener("activate", e => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+    ).then(() => self.clients.claim())
+  );
+});
 self.addEventListener("fetch", e => {
-  e.respondWith(caches.match(e.request).then(r => r || fetch(e.request)));
+  e.respondWith(fetch(e.request).catch(() => caches.match(e.request)));
 });
